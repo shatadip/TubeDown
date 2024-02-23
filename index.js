108998297181001ca80a6329673c169280df5238
Shatadip Majumder <shatadip.majumder@gmail.com>
2024-02-23 23:32:57 +0530

    Basic Server Created to Download YouTube videos @highest-quality

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const ytdl = require("ytdl-core");
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
+
+app.get("/download", (req, res) => {
+    const url = req.query.url;
+    if (ytdl.validateURL(url)) {
+        ytdl.getInfo(url).then(info => {
+            // Filter the formats to get the highest quality video
+            const format = ytdl.chooseFormat(info.formats, { quality: 'highest' });
+            if (format) {
+                res.header('Content-Disposition', `attachment; filename="${info.videoDetails.title}.mp4"`);
+                ytdl(url, { format: format }).pipe(res);
+            } else {
+                res.status(404).send('No video found');
+            }
+        }).catch(err => {
+            res.status(500).send('Failed to get video information');
+        });
+    } else {
+        res.status(400).send('Invalid YouTube URL');
+    }
+});
