7a1486adaa91bf96bcc58d6b08a4a5513fccaf0b
Shatadip Majumder <shatadip.majumder@gmail.com>
2024-02-26 23:53:39 +0530

    Added a page to paste & test YT URLs

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
 
+app.set('view engine', 'ejs');
+app.get("/", (req, res) => {
+    res.render('index');
+});
+
 app.get("/download", (req, res) => {
     const url = req.query.url;
     if (ytdl.validateURL(url)) {
